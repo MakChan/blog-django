0022fea26cbb4bc27dfc6a11c1eae5b3d269b722
Mayank Chauhan <mayank.chauhan15@gmail.com>
2018-03-27 23:26:24 +0530

    Add new views and edit templates

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
-    path('blog/<int:pk>/', views.PostView.as_view(), name='post')
+    path('blog/<int:pk>/', views.PostView.as_view(), name='post'),
+    path('blogger/<username>/', views.UserView.as_view(), name='blogger'),
+    path('bloggers/', views.UserListView.as_view(), name='bloggers')
 ]
