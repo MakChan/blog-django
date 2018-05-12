f9832ff4216b5b793715c6025eb80c0061beeb21
Mayank Chauhan <mayank.chauhan15@gmail.com>
2018-05-12 23:48:54 +0530

    Improve UI

--- project_blog/settings.py
+++ project_blog/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/blog/static/'
 
 AUTH_USER_MODEL = 'accounts.User'
 
--- project_blog/wsgi.py
+++ project_blog/wsgi.py
@@ -7,7 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+
+sys.path.append('./')
+sys.path.append('./env/lib/python3.5/site-packages')
+
 
 from django.core.wsgi import get_wsgi_application
 
