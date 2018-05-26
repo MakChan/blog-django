690b926ee818080f41f11ebd360eed836f4a8e0b
Mayank Chauhan <mayank.chauhan15@gmail.com>
2018-05-26 23:45:05 +0530

    Add gravatar, change UI

--- accounts/models.py
+++ accounts/models.py
@@ -4,5 +4,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     bio = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=50, null=True)
     is_blogger = models.BooleanField(default = False)
 
